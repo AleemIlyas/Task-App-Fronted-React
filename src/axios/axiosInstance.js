080405106cbceed3a2ac0bbd8d5c433f03cd8330
Aleem Ilyas <aleemempire215@gmail.com>
2024-07-12 16:53:29 +0500

    update backend api address

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const AxiosInstance = axios.create({
-    baseURL : 'https://light-slug-slacks.cyclic.app',
+    baseURL: 'https://taskapp-aleemilyas.azurewebsites.net/',
 })
 
 export default AxiosInstance
